c981fd19ed3ae3b1c49f0206843872fba50775b7
peymancyb <paymanghazvini@yahoo.com>
2018-01-29 23:11:24 +0400

    function calls

--- functions.js
+++ functions.js
@@ -20,3 +20,30 @@ function factorial(width,height){
 //function declerations are hoisted
 //but
 //function expression are not hoisted
+
+
+//in arrow functions , one line statements are considered as
+//expression function
+
+// now lets see what are the differences of calling functions
+
+functionName.prototype.name = "peyman";
+
+function functionName(z){
+  console.log(this.name);
+  let x = "there is no losing only learning";
+  if(z!==undefined){
+    return console.log(x+" "+z);
+  }else
+    return console.log(x);
+
+}
+
+//calling function
+functionName();
+//calling function and make prototype for the function
+new functionName();
+//apply method pass can bind this, and pass values in an array as arguments
+functionName.apply(this,["hi"]);
+//call method pass can bind this, and pass values in array as arguments
+functionName.call(this,"hi");
