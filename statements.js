ff3113ce2b67adc06f7f1ed16a47ef4d209120d5
peymancyb <paymanghazvini@yahoo.com>
2018-02-05 23:46:23 +0400

    setImmediate deprecated

--- statements.js
+++ statements.js
@@ -9,3 +9,23 @@ try{
 }catch(e){
     console.log(`error is : ${e}`)
 }
+
+
+
+
+
+// Non-standard
+// This feature is non-standard and is not on a standards track. Do not use it on
+// production sites facing the Web: it will not work for every user. There may also
+// be large incompatibilities between implementations and the behavior may change
+// in the future.
+// This method is used to break up long running operations and run a callback
+// function immediately after the browser has completed other operations such
+// as events and display updates.
+
+//
+// syntax
+// var immediateID = setImmediate(func);
+
+
+var immediateID = setImmediate(()=>console.log("call back immediately"));
